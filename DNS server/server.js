5dbce273583fb3d2761bceab11fac8de96f4c307
Eben <96250954+EbenJesusSaves@users.noreply.github.com>
2024-05-29 22:08:07 +0000

    server implementation

--- DNS server/server.js
+++ DNS server/server.js
@@ -1,7 +1,12 @@
 import dgram from "dgram";
 
-import { processBindFile } from ".";
+import { processBindFile } from "./index.js";
 
 const server = dgram.createSocket("udp4");
 
 server.bind("53");
+
+const king = "boy ";
+
+king.toString(16);
+console.log(king);
